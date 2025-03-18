e8c3b24e8bb9ee9085a171aee16f50e59cae9c2d
nydragon <contact@ccnlc.eu>
2025-03-18 22:07:50 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    {
+
+      packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.callPackage ./package.nix { };
+
+    };
+}
